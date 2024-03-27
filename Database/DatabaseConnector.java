262cde967a5c180c53ab71db3a40cbaa2fc025f8
Tijn Van de Velde <70897975+TijnVdVelde@users.noreply.github.com>
2024-03-27 12:33:29 +0100

    ! potential fix database connection

--- Database/DatabaseConnector.java
+++ Database/DatabaseConnector.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnector {
-    private static final String URL = "jdbc:sqlserver://aei-sql2.avans.nl:1443;databaseName=CodeCademyGroupC3;encrypt=true;trustServerCertificate=true";
+    private static final String URL = "jdbc:sqlserver://aei-sql2.avans.nl:1443;databaseName=CodeCademyGroupC3;trustServerCertificate=true";
     private static final String USER = "userTJES";
     private static final String PASSWORD = "GroupC3";
 
@@ -17,10 +17,12 @@ public class DatabaseConnector {
             System.out.println("SQLException: " + e.getMessage());
             System.out.println("SQLState: " + e.getSQLState());
             System.out.println("VendorError: " + e.getErrorCode());
+            e.printStackTrace(); // Add stack trace for more details
             return null;
         } catch (ClassNotFoundException e) {
             System.out.println("JDBC Driver not found: " + e.getMessage());
+            e.printStackTrace(); // Add stack trace for more details
             return null;
-        }
+        }        
     }
 }
